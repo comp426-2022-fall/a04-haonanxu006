43928408e650c0d9dd208d87b9ada44fd8ae45f6
Haonan Xu <69841982+haonanxu006@users.noreply.github.com>
2022-11-22 12:21:19 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,41 @@
+import roll from "./lib/roll.js"
+import minimist from 'minimist'
+import express from "express"
+import res from "express/lib/response.js";
+
+const args = minimist(process.argv.slice(2));
+const port = args.port || 5000;
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+
+
+app.get('/app', (req, res) => {
+    res.send("200 OK");
+})
+
+app.get('/app/roll', (req, res) => {
+    res.send(roll(6, 2, 1));
+})
+
+app.post('/app/roll', (req, res) => {
+    res.send(roll(parseInt(req.body.sides), parseInt(req.body.dice), parseInt(req.body.rolls)));
+})
+
+app.get('/app/roll/:sides/', (req, res) => {
+    res.send(roll(parseInt(req.params.sides), 2, 1));
+})
+
+app.get('/app/roll/:sides/:dice/', (req, res) => {
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), 1));
+})
+
+app.get('/app/roll/:sides/:dice/:rolls/', (req, res) => {
+    res.send(roll(parseInt(req.params.sides), parseInt(req.params.dice), parseInt(req.params.rolls)));
+})
+
+app.get('/app/*', (req, res) => {
+    res.send("404 NOT FOUND");
+})
+
+app.listen(port);
